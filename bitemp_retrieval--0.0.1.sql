ef6c7be1b89616694168a49dbb7bb0e3b8db7c82
Aditya Bimawan <aditditto@gmail.com>
2023-07-24 22:26:38 +0700

    rename bitemp join

--- bitemp_retrieval--0.0.1.sql
+++ bitemp_retrieval--0.0.1.sql
@@ -65,7 +65,7 @@ $BODY$
 LANGUAGE plpgsql;
 
 CREATE OR REPLACE
-FUNCTION bitemp_timeperiod_joinable(
+FUNCTION bitemp_joinable(
     a temporal_relationships.timeperiod,
     b temporal_relationships.timeperiod
     ) 
@@ -79,7 +79,7 @@ $BODY$
 LANGUAGE plpgsql;
 
 CREATE OR REPLACE
-FUNCTION bitemp_timeperiod_join(
+FUNCTION bitemp_join(
     a temporal_relationships.timeperiod,
     b temporal_relationships.timeperiod
     ) 
