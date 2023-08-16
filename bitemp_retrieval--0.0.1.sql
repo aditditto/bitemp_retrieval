86217100117f75764a41a9ea95b6f1bb248e6b3e
Aditya Bimawan <aditditto@gmail.com>
2023-08-16 20:46:02 +0700

    refactor coalesce to plv8

--- bitemp_retrieval--0.0.1.sql
+++ bitemp_retrieval--0.0.1.sql
@@ -118,59 +118,70 @@ FUNCTION unitemp_coalesce_select(
 RETURNS SETOF RECORD
 AS
 $body$
-    my ($p_query, $p_list_of_fields_ref, $p_temporal_field) = @_;
+  function parseISOString(s) {
+    var b = s.split(/\D+/);
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  }
 
-    my @list_of_fields = @{$p_list_of_fields_ref};
-    my $fields_string = join(",", map({ "\"$_\"" } @list_of_fields));
+  plv8.execute("SET DATESTYLE TO 'ISO'");
+  let fields_string = p_list_of_fields.map((field) => `"${field}"`).join(",");
+  let query = `${p_query} ORDER BY ${fields_string}, LOWER("${p_temporal_field}")`;
+  let plan = plv8.prepare(query);
+  let cursor = plan.cursor();
 
-    my $query = "$p_query ORDER BY $fields_string, \"$p_temporal_field\"";
-    my $sth = spi_query($query);
+  let curr_row, to_coalesce, coalesce_start, coalesce_end, endpoints;
 
-    my $curr_row;
-    my $to_coalesce = undef;
-    my $count = 0;
-    my $coalesce_start = undef;
-    my $coalesce_end = undef;
-    my @endpoints = undef;
+  while ((curr_row = cursor.fetch())) {
+    endpoints = curr_row[p_temporal_field].slice(1, -1).split(",");
 
-    while (defined ($curr_row = spi_fetchrow($sth))) {
-        # elog(NOTICE, "curr_row = $curr_row to_coalesce  = $to_coalesce");
-        my @endpoints = split(',',substr($curr_row->{$p_temporal_field}, 1, -1));
-
-        if(!(defined $to_coalesce)) {
-            # elog(NOTICE, "HELLO HELLO FIRST LOOP");
-            $to_coalesce = $curr_row;
-            # elog(NOTICE, "to_coalesce  = $to_coalesce");
-            $coalesce_start = $endpoints[0];
-            $coalesce_end = $endpoints[1];
-        } else {
-            my $equal = 1;
-            ISEQ: foreach my $field (@list_of_fields) {
-                if ($curr_row->{$field} ne $to_coalesce->{$field}) {
-                    $equal = 0;
-                    last ISEQ;
-                }
-            }
-
-            if ($equal && ($coalesce_end eq $endpoints[0])) {
-                $coalesce_end = $endpoints[1];
-            } else {
-                $to_coalesce->{$p_temporal_field} = "[$coalesce_start,$coalesce_end)";
-                return_next($to_coalesce);
-
-                $to_coalesce = $curr_row;
-                $coalesce_start = $endpoints[0];
-                $coalesce_end = $endpoints[1];
-            }
+    if (!to_coalesce) {
+      to_coalesce = curr_row;
+      coalesce_start = endpoints[0];
+      coalesce_end = endpoints[1];
+    } else {
+      let equal = true;
+      for (field of p_list_of_fields) {
+        if (curr_row[field] != to_coalesce[field]) {
+          equal = false;
         }
+      }
+
+      if (equal) {
+        if (coalesce_end != "infinity") {
+          let coalesce_end_date = parseISOString(coalesce_end);
+          let curr_start_date = parseISOString(endpoints[0]);
+
+          if (curr_start_date <= coalesce_end_date) {
+            coalesce_end = endpoints[1];
+          } else {
+            to_coalesce[
+              p_temporal_field
+            ] = `[${coalesce_start},${coalesce_end})`;
+            plv8.return_next(to_coalesce);
+
+            to_coalesce = curr_row;
+            coalesce_start = endpoints[0];
+            coalesce_end = endpoints[1];
+          }
+        }
+      } else {
+        to_coalesce[p_temporal_field] = `[${coalesce_start},${coalesce_end})`;
+        plv8.return_next(to_coalesce);
+
+        to_coalesce = curr_row;
+        coalesce_start = endpoints[0];
+        coalesce_end = endpoints[1];
+      }
     }
+  }
 
-    $to_coalesce->{$p_temporal_field} = "[$coalesce_start,$coalesce_end)";
-    return_next($to_coalesce);
+  to_coalesce[p_temporal_field] = `[${coalesce_start},${coalesce_end})`;
+  plv8.return_next(to_coalesce);
 
-    return undef;
+  cursor.close();
+  plan.free();
 $body$
-LANGUAGE plperl;
+LANGUAGE plv8;
 
 
 CREATE OR REPLACE
@@ -181,60 +192,71 @@ FUNCTION unitemp_coalesce_select_effective(
 RETURNS SETOF RECORD
 AS
 $body$
-    my ($p_query, $p_list_of_fields_ref) = @_;
-    $p_temporal_field = 'effective';
+  function parseISOString(s) {
+    var b = s.split(/\D+/);
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  }
 
-    my @list_of_fields = @{$p_list_of_fields_ref};
-    my $fields_string = join(",", map({ "\"$_\"" } @list_of_fields));
+  plv8.execute("SET DATESTYLE TO 'ISO'");
+  let p_temporal_field = "effective";
+  let fields_string = p_list_of_fields.map((field) => `"${field}"`).join(",");
+  let query = `${p_query} ORDER BY ${fields_string}, LOWER("${p_temporal_field}")`;
+  let plan = plv8.prepare(query);
+  let cursor = plan.cursor();
 
-    my $query = "$p_query ORDER BY $fields_string, \"$p_temporal_field\"";
-    my $sth = spi_query($query);
+  let curr_row, to_coalesce, coalesce_start, coalesce_end, endpoints;
 
-    my $curr_row;
-    my $to_coalesce = undef;
-    my $count = 0;
-    my $coalesce_start = undef;
-    my $coalesce_end = undef;
-    my @endpoints = undef;
+  while ((curr_row = cursor.fetch())) {
+    endpoints = curr_row[p_temporal_field].slice(1, -1).split(",");
 
-    while (defined ($curr_row = spi_fetchrow($sth))) {
-        # elog(NOTICE, "curr_row = $curr_row to_coalesce  = $to_coalesce");
-        my @endpoints = split(',',substr($curr_row->{$p_temporal_field}, 1, -1));
-
-        if(!(defined $to_coalesce)) {
-            # elog(NOTICE, "HELLO HELLO FIRST LOOP");
-            $to_coalesce = $curr_row;
-            # elog(NOTICE, "to_coalesce  = $to_coalesce");
-            $coalesce_start = $endpoints[0];
-            $coalesce_end = $endpoints[1];
-        } else {
-            my $equal = 1;
-            ISEQ: foreach my $field (@list_of_fields) {
-                if ($curr_row->{$field} ne $to_coalesce->{$field}) {
-                    $equal = 0;
-                    last ISEQ;
-                }
-            }
-
-            if ($equal && ($coalesce_end eq $endpoints[0])) {
-                $coalesce_end = $endpoints[1];
-            } else {
-                $to_coalesce->{$p_temporal_field} = "[$coalesce_start,$coalesce_end)";
-                return_next($to_coalesce);
-
-                $to_coalesce = $curr_row;
-                $coalesce_start = $endpoints[0];
-                $coalesce_end = $endpoints[1];
-            }
+    if (!to_coalesce) {
+      to_coalesce = curr_row;
+      coalesce_start = endpoints[0];
+      coalesce_end = endpoints[1];
+    } else {
+      let equal = true;
+      for (field of p_list_of_fields) {
+        if (curr_row[field] != to_coalesce[field]) {
+          equal = false;
         }
+      }
+
+      if (equal) {
+        if (coalesce_end != "infinity") {
+          let coalesce_end_date = parseISOString(coalesce_end);
+          let curr_start_date = parseISOString(endpoints[0]);
+
+          if (curr_start_date <= coalesce_end_date) {
+            coalesce_end = endpoints[1];
+          } else {
+            to_coalesce[
+              p_temporal_field
+            ] = `[${coalesce_start},${coalesce_end})`;
+            plv8.return_next(to_coalesce);
+
+            to_coalesce = curr_row;
+            coalesce_start = endpoints[0];
+            coalesce_end = endpoints[1];
+          }
+        }
+      } else {
+        to_coalesce[p_temporal_field] = `[${coalesce_start},${coalesce_end})`;
+        plv8.return_next(to_coalesce);
+
+        to_coalesce = curr_row;
+        coalesce_start = endpoints[0];
+        coalesce_end = endpoints[1];
+      }
     }
+  }
 
-    $to_coalesce->{$p_temporal_field} = "[$coalesce_start,$coalesce_end)";
-    return_next($to_coalesce);
+  to_coalesce[p_temporal_field] = `[${coalesce_start},${coalesce_end})`;
+  plv8.return_next(to_coalesce);
 
-    return undef;
+  cursor.close();
+  plan.free();
 $body$
-LANGUAGE plperl;
+LANGUAGE plv8;
 
 CREATE OR REPLACE
 FUNCTION unitemp_coalesce_table_effective(
@@ -245,65 +267,75 @@ FUNCTION unitemp_coalesce_table_effective(
 RETURNS SETOF RECORD
 AS
 $body$
-    my ($p_schema, $p_table, $p_list_of_fields_ref) = @_;
+  function parseISOString(s) {
+    var b = s.split(/\D+/);
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  }
 
-    my @list_of_fields = @{$p_list_of_fields_ref};
-    my $fields_string = join(",", map({ "\"$_\"" } @list_of_fields));
-    my $p_temporal_field = 'effective';
+  plv8.execute("SET DATESTYLE TO 'ISO'");
+  let p_temporal_field = "effective";
+  let fields_string = p_list_of_fields.map((field) => `"${field}"`).join(",");
+  let query = `SELECT ${fields_string}, \"${p_temporal_field}\" FROM \"${p_schema}\".\"${p_table}\" WHERE now() <@ asserted ORDER BY ${fields_string}, \"${p_temporal_field}\"`;
+  let plan = plv8.prepare(query);
+  let cursor = plan.cursor();
 
-    my $query = "SELECT $fields_string, \"$p_temporal_field\" FROM \"$p_schema\".\"$p_table\" WHERE now() <@ asserted ORDER BY $fields_string, \"$p_temporal_field\"";
-    # elog(NOTICE, $query);
-    my $sth = spi_query($query);
+  let curr_row, to_coalesce, coalesce_start, coalesce_end, endpoints;
 
-    my $curr_row;
-    my $to_coalesce = undef;
-    my $count = 0;
-    my $coalesce_start = undef;
-    my $coalesce_end = undef;
-    my @endpoints = undef;
+  while ((curr_row = cursor.fetch())) {
+    endpoints = curr_row[p_temporal_field].slice(1, -1).split(",");
 
-    while (defined ($curr_row = spi_fetchrow($sth))) {
-        # elog(NOTICE, "curr_row = $curr_row to_coalesce  = $to_coalesce");
-        my @endpoints = split(',',substr($curr_row->{$p_temporal_field}, 1, -1));
-
-        if(!(defined $to_coalesce)) {
-            # elog(NOTICE, "HELLO HELLO FIRST LOOP");
-            $to_coalesce = $curr_row;
-            # elog(NOTICE, "to_coalesce  = $to_coalesce");
-            $coalesce_start = $endpoints[0];
-            $coalesce_end = $endpoints[1];
-        } else {
-            my $equal = 1;
-            ISEQ: foreach my $field (@list_of_fields) {
-                if ($curr_row->{$field} ne $to_coalesce->{$field}) {
-                    $equal = 0;
-                    last ISEQ;
-                }
-            }
-
-            if ($equal && ($coalesce_end eq $endpoints[0])) {
-                $coalesce_end = $endpoints[1];
-            } else {
-                $to_coalesce->{$p_temporal_field} = "[$coalesce_start,$coalesce_end)";
-                return_next($to_coalesce);
-
-                $to_coalesce = $curr_row;
-                $coalesce_start = $endpoints[0];
-                $coalesce_end = $endpoints[1];
-            }
+    if (!to_coalesce) {
+      to_coalesce = curr_row;
+      coalesce_start = endpoints[0];
+      coalesce_end = endpoints[1];
+    } else {
+      let equal = true;
+      for (field of p_list_of_fields) {
+        if (curr_row[field] != to_coalesce[field]) {
+          equal = false;
         }
+      }
+
+      if (equal) {
+        if (coalesce_end != "infinity") {
+          let coalesce_end_date = parseISOString(coalesce_end);
+          let curr_start_date = parseISOString(endpoints[0]);
+
+          if (curr_start_date <= coalesce_end_date) {
+            coalesce_end = endpoints[1];
+          } else {
+            to_coalesce[
+              p_temporal_field
+            ] = `[${coalesce_start},${coalesce_end})`;
+            plv8.return_next(to_coalesce);
+
+            to_coalesce = curr_row;
+            coalesce_start = endpoints[0];
+            coalesce_end = endpoints[1];
+          }
+        }
+      } else {
+        to_coalesce[p_temporal_field] = `[${coalesce_start},${coalesce_end})`;
+        plv8.return_next(to_coalesce);
+
+        to_coalesce = curr_row;
+        coalesce_start = endpoints[0];
+        coalesce_end = endpoints[1];
+      }
     }
+  }
 
-    $to_coalesce->{$p_temporal_field} = "[$coalesce_start,$coalesce_end)";
-    return_next($to_coalesce);
+  to_coalesce[p_temporal_field] = `[${coalesce_start},${coalesce_end})`;
+  plv8.return_next(to_coalesce);
 
-    return undef;
+  cursor.close();
+  plan.free();
 $body$
-LANGUAGE plperl;
+LANGUAGE plv8;
 
 
 
-CREATE FUNCTION v8_select_test(p_query TEXT) RETURNS void AS 
+CREATE FUNCTION v8_select_test(p_query TEXT, p_interval INTERVAL) RETURNS void AS 
 $$
     var avl = plv8.execute("select source from bitemporal_internal.bitemp_retrieval_utils where module = 'AVLTree'");
     eval(avl[0].source);
@@ -312,6 +344,11 @@ $$
     plv8.elog(INFO, minfinity[0].upper.getTime());
     plv8.elog(INFO, minfinity[0].lower > Date.now());
     plv8.elog(INFO, minfinity[0].lower < Date.now());
+    var tsrange = plv8.execute(`select '["-infinity", "infinity"]'::tstzrange AS test`);
+    plv8.elog(INFO, tsrange[0].test);
+    plv8.elog(INFO, typeof tsrange[0].test);
+    plv8.elog(INFO, plv8.execute(`select extract(epoch from interval '${p_interval}')`)[0].extract);
+    plv8.elog(INFO, typeof p_interval);
 $$
 LANGUAGE plv8;
 
---
+++ dev/coalesce.js
@@ -0,0 +1,66 @@
+function coalesce_select(p_query, p_list_of_fields, p_temporal_field) {
+  function parseISOString(s) {
+    var b = s.split(/\D+/);
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  }
+
+  plv8.execute("SET DATESTYLE TO 'ISO'");
+  let fields_string = p_list_of_fields.map((field) => `"${field}"`).join(",");
+  let query = `${p_query} ORDER BY ${fields_string}, LOWER("${p_temporal_field}")`;
+  let plan = plv8.prepare(query);
+  let cursor = plan.cursor();
+
+  let curr_row, to_coalesce, coalesce_start, coalesce_end, endpoints;
+
+  while ((curr_row = cursor.fetch())) {
+    endpoints = curr_row[p_temporal_field].slice(1, -1).split(",");
+
+    if (!to_coalesce) {
+      to_coalesce = curr_row;
+      coalesce_start = endpoints[0];
+      coalesce_end = endpoints[1];
+    } else {
+      let equal = true;
+      for (field of p_list_of_fields) {
+        if (curr_row[field] != to_coalesce[field]) {
+          equal = false;
+        }
+      }
+
+      if (equal) {
+        if (coalesce_end != "infinity") {
+          let coalesce_end_date = parseISOString(coalesce_end);
+          let curr_start_date = parseISOString(endpoints[0]);
+
+          if (curr_start_date <= coalesce_end_date) {
+            coalesce_end = endpoints[1];
+          } else {
+            to_coalesce[
+              p_temporal_field
+            ] = `[${coalesce_start},${coalesce_end})`;
+            plv8.return_next(to_coalesce);
+
+            to_coalesce = curr_row;
+            coalesce_start = endpoints[0];
+            coalesce_end = endpoints[1];
+          }
+        }
+      } else {
+        to_coalesce[p_temporal_field] = `[${coalesce_start},${coalesce_end})`;
+        plv8.return_next(to_coalesce);
+
+        to_coalesce = curr_row;
+        coalesce_start = endpoints[0];
+        coalesce_end = endpoints[1];
+      }
+    }
+  }
+
+  to_coalesce[p_temporal_field] = `[${coalesce_start},${coalesce_end})`;
+  plv8.return_next(to_coalesce);
+
+  cursor.close();
+  plan.free();
+}
+
+let table_query = `SELECT ${fields_string}, \"${p_temporal_field}\" FROM \"${p_schema}\".\"${p_table}\" WHERE now() <@ asserted ORDER BY ${fields_string}, \"${p_temporal_field}\"`;
